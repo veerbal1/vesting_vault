41e46e78f18c45356e5ac07e43c2eb416cce4768
veerbal1 <veerbalsingh1@gmail.com>
2025-11-09 14:47:56 +0530

    Add vesting vault program with token transfer functionality
    - Introduced `vesting_vault` program with initialization functions for vault and vesting accounts.
    - Integrated `anchor-spl` for token handling.
    - Updated `Cargo.toml` to include `anchor-spl` dependency and modified `anchor-lang` version.
    - Added error handling for vault token depletion.
    - Defined data structures for `VestingAccount` and `VaultState` with appropriate initialization logic.

--- programs/vesting_vault/src/lib.rs
+++ programs/vesting_vault/src/lib.rs
@@ -1,16 +1,136 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
 
 declare_id!("GvzD2zDi4AvLjRA5893csKVsHYezfv6D2B3SpAenSxoi");
 
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Vault of out tokens")]
+    VaultOutOfToken,
+}
+
 #[program]
 pub mod vesting_vault {
+
+    use anchor_spl::token;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_vault(ctx: Context<InitializeVault>) -> Result<()> {
+        let vault_state = &mut ctx.accounts.vault_state;
+        vault_state.vault_token_account = ctx.accounts.vault_token_account.key();
+        vault_state.vault_bump = ctx.bumps.vault_token_account;
+        vault_state.mint = ctx.accounts.mint.key();
+        Ok(())
+    }
+
+    pub fn initialize_vesting(
+        ctx: Context<InitializeVesting>,
+        beneficiary: Pubkey,
+        token_tokens: u64,
+        end_at: i64,
+        cliff_period_till: i64,
+    ) -> Result<()> {
+        let current_time = Clock::get()?.unix_timestamp;
+        let vesting_account = &mut ctx.accounts.vesting_account;
+
+        vesting_account.beneficiary = beneficiary;
+        vesting_account.total_tokens = token_tokens;
+        vesting_account.started_at = current_time;
+        vesting_account.end_at = end_at;
+        vesting_account.claimed_tokens = 0;
+        vesting_account.cliff_period_till = cliff_period_till;
+
+        vesting_account.bump = ctx.bumps.vesting_account;
+
+        // CPI
+        let vault_token_account = &ctx.accounts.vault_token_account;
+
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.admin_token_account.to_account_info(),
+            to: vault_token_account.to_account_info(),
+            authority: ctx.accounts.admin.to_account_info(),
+        };
+
+        let token_program = ctx.accounts.token_program.to_account_info();
+
+        let cpi_context = CpiContext::new(token_program, cpi_accounts);
+
+        token::transfer(cpi_context, token_tokens)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(beneficiary: Pubkey)]
+pub struct InitializeVesting<'info> {
+    #[account(init, seeds=[b"vesting", beneficiary.key().as_ref()], bump, payer = admin, space = VestingAccount::LEN)]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    pub mint: Account<'info, Mint>,
+
+    #[account(mut, token::mint = mint, token::authority = admin)]
+    pub admin_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        seeds = [b"vault"],
+        bump,
+        token::mint = mint,
+        token::authority = vault_token_account,
+        payer = admin
+    )]
+    pub vault_token_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VestingAccount {
+    pub beneficiary: Pubkey,
+    pub total_tokens: u64,
+    pub started_at: i64,
+    pub end_at: i64,
+    pub claimed_tokens: u64,
+    pub cliff_period_till: i64,
+    pub bump: u8,
+}
+
+impl VestingAccount {
+    pub const LEN: usize = 8 + Self::INIT_SPACE;
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VaultState {
+    pub vault_token_account: Pubkey,
+    pub vault_bump: u8,
+    pub mint: Pubkey,
+}
+
+impl VaultState {
+    pub const LEN: usize = 8 + Self::INIT_SPACE;
+}
+
+#[derive(Accounts)]
+pub struct InitializeVault<'info> {
+    #[account(init, seeds = [b"vault_state"], bump, payer = admin, space = VaultState::LEN)]
+    pub vault_state: Account<'info, VaultState>,
+
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    pub mint: Account<'info, Mint>,
+
+    #[account(init_if_needed, seeds = [b"vault"], bump, token::mint = mint, token::authority = vault_token_account, payer = admin)]
+    pub vault_token_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
